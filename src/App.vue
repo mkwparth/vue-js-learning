a69dc044af7defd7bca6d6e916dd40d5a58da578
MAKWANA PARTH ASHOKBHAI <84260254+mkwparth@users.noreply.github.com>
2024-08-27 22:53:08 +0530

    setup-composition-api-app2-file

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script>
+  export default{
+    data(){
+      return{
+        name:'Parth Makwana',
+        status:false,
+        items:['parth','Mohan','varun'],
+        link:"https://google.com"
+      }
+    },
+    methods: {
+      toggleStatus(){
+        this.status = !this.status
+      }
+    },
+  }
+</script>
+
+<template>
+  <p>{{ name }}</p>
+  <p v-if="status">User is active</p>
+  <p v-else>User is not active</p>
+  <ul>
+    <li v-for="item in items">{{ item }}</li>
+  </ul>
+  <!-- <p v-for="item in items">{{ item }}</p> -->
+   <a :href="link">Click for google</a> <!-- we can use v-bind as : this way too.--> 
+    <br><button @click="toggleStatus">Toggle Status</button>
+</template>
---
+++ src/App2.vue
@@ -0,0 +1,26 @@
+<script setup>
+  import { ref } from 'vue';
+   
+  //data
+  const name = ref('Parth Makwana');
+  const status = ref(true);
+  const items = ref(['parth','mohan','varun'])
+
+  //methods
+  const toggleStatus = ()=>{
+    status.value = !status.value;
+  }
+
+</script>
+
+<template>
+  <p>{{ name }}</p>
+  <p v-if="status">User is active</p>
+  <p v-else>User is not active</p>
+  <ul>
+    <li v-for="item in items">{{ item }}</li>
+  </ul>
+  <!-- <p v-for="item in items">{{ item }}</p> -->
+   <a :href="link">Click for google</a> <!-- we can use v-bind as : this way too.--> 
+    <br><button @click="toggleStatus">Toggle Status</button>
+</template>
