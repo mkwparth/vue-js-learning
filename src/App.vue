1355c1d992e20569fd23a24309ea5b0e7f83a728
Parth Makwana <makwanaparth1820@gmail.com>
2024-10-12 23:26:06 +0530

    Job listings UI is complete

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <script setup lang="ts">
-  import Navbar from './components/Navbar.vue';
+import Hero from './components/Hero.vue';
+import HomeCards from './components/HomeCards.vue';
+import JobListings from './components/JobListings.vue';
+import Navbar from './components/Navbar.vue';
 </script>
 
 <template>
   <Navbar />
+  <Hero />
+  <HomeCards />
+  <JobListings :limit="3" :showButton="true" />
 </template>
