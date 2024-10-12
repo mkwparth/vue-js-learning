c6b3e58dbd378cf9a18202fc3574f59f164b9b69
Parth Makwana <makwanaparth1820@gmail.com>
2024-10-12 17:51:25 +0530

    Setup tailwind css

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,3 @@
-<script>
-  export default{
-    data(){
-      return{
-        name:'Parth Makwana',
-        status:false,
-        items:['parth','Mohan','varun'],
-        link:"https://google.com"
-      }
-    },
-    methods: {
-      toggleStatus(){
-        this.status = !this.status
-      }
-    },
-  }
-</script>
-
 <template>
-  <p>{{ name }}</p>
-  <p v-if="status">User is active</p>
-  <p v-else>User is not active</p>
-  <ul>
-    <li v-for="item in items">{{ item }}</li>
-  </ul>
-  <!-- <p v-for="item in items">{{ item }}</p> -->
-   <a :href="link">Click for google</a> <!-- we can use v-bind as : this way too.--> 
-    <br><button @click="toggleStatus">Toggle Status</button>
+  <h1 class="text-2xl" >Hello I'm Parth</h1>
 </template>
