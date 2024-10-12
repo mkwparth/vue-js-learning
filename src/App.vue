505a51613673fd9ce8bc820dc5b7fcdfe870bf02
MAKWANA PARTH ASHOKBHAI <84260254+mkwparth@users.noreply.github.com>
2024-10-12 16:39:29 +0530

    Add this file

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
+<script setup>
+import { reactive } from 'vue';
+
+
+const state = reactive({count:0})
+
+</script>
+
 <template>
   <p>Hello This Parth Makwana</p>
+  <button @click="state.count++">
+    Increase No
+  </button>
+  
+  <p>
+    {{ state.count }}
+  </p>
+
 </template>
-
-<script>
-
-export default {
-  name: 'App',
-  components: {
-    
-  }
-}
-</script>
 
---
+++ src/components/FormComponent.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from 'vue';
+
+   const tasks = ref([]);     
+   const newTask = ref('BC '); 
+   const addTask = ()=>{
+    if(newTask.value.trim() !== ''){
+        tasks.value.push(newTask.value);
+    }
+    newTask.value = '';
+   } 
+</script>
+
+<template>
+    Form component
+    <form @submit.prevent="addTask">
+        <label for="newTask">Add task</label>
+        <input type="text" id="newText" name="newTask" v-model="newTask"/>
+
+        <button type="submit">Submit</button>
+        <p v-for="task in tasks" :key="task">{{task.value}}</p>
+        
+    </form>
+
+</template>
