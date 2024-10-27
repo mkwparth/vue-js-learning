d695183ff524c288a42a76739a12884772e1498a
Parth Makwana <makwanaparth1820@gmail.com>
2024-10-27 17:50:55 +0530

    Implement Readmore button in jobview

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -1,8 +1,9 @@
 <script setup>
-import jobData from '@/jobs.json'
-import { ref, defineProps } from 'vue';
+import { defineProps, onMounted, reactive } from 'vue';
+import PulseLoader from 'vue-spinner/src/PulseLoader.vue'
 import JobListing from './JobListing.vue';
 import { RouterLink } from 'vue-router';
+import axios from 'axios';
 
 defineProps({
     limit: Number,
@@ -11,7 +12,21 @@ defineProps({
         default: false
     }
 })
-const jobs = ref(jobData)
+const state = reactive({
+    jobs: [],
+    isLoading: true
+});
+
+onMounted(async () => {
+    try {
+        const response = await axios.get('http://localhost:5000/jobs');
+        state.jobs = response.data;
+    } catch (error) {
+        console.log('Error fetching error', error);
+    } finally {
+        state.isLoading = false;
+    }
+})
 </script>
 <template>
     <section class="bg-blue-50 px-4 py-10">
@@ -19,8 +34,13 @@ const jobs = ref(jobData)
             <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
                 Browse Jobs
             </h2>
-            <div class="grid grid-col-1 md:grid-cols-3 gap-6">
-                <JobListing v-for="job in jobs.slice(0, limit || jobs.length)" :key="job.id" :job="job" />
+            <!-- show loadin spinner while loading is true -->
+            <div v-if="state.isLoading" class="text-center text-gray-500 py-6">
+                <PulseLoader />
+            </div>
+            <!-- show listing when it's done loading -->
+            <div v-else class="grid grid-col-1 md:grid-cols-3 gap-6">
+                <JobListing v-for="job in state.jobs.slice(0, limit || state.jobs.length)" :key="job.id" :job="job" />
             </div>
         </div>
     </section>
