8976b495404759d83dc9a1ee024904088e8c56d6
Parth Makwana <makwanaparth1820@gmail.com>
2024-10-27 11:54:35 +0530

    Added jobview page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import HomeView from "@/views/HomeView.vue";
 import JobAddView from "@/views/JobAddView.vue";
 import JobsView from "@/views/JobsView.vue";
+import JobView from "@/views/JobView.vue";
+import NotFoundView from "@/views/NotFoundView.vue";
 import { createRouter, createWebHashHistory } from "vue-router";
 
 const router = createRouter({
@@ -20,6 +22,16 @@ const router = createRouter({
             path: '/jobs/add',
             name: 'jobAdd',
             component: JobAddView,
+        },
+        {
+            path: '/jobs/:id',
+            name: 'job-view',
+            component: JobView,
+        },
+        {
+            path: '/:catchAll(.*)',
+            name: 'not-found',
+            component: NotFoundView,
         },
     ]
 })
