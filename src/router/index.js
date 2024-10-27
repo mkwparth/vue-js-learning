a869cf5895d453b1b3214149452c6c19de59ad0c
Parth Makwana <makwanaparth1820@gmail.com>
2024-10-27 11:07:06 +0530

    Replace a tag with routerLink

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,25 @@
 import HomeView from "@/views/HomeView.vue";
+import JobAddView from "@/views/JobAddView.vue";
+import JobsView from "@/views/JobsView.vue";
 import { createRouter, createWebHashHistory } from "vue-router";
 
 const router = createRouter({
-    history: createWebHashHistory(import.meta.env.BASE_URL),
+    history: createWebHashHistory(),
     routes: [
         {
             path: '/',
             name: 'home',
             component: HomeView,
+        },
+        {
+            path: '/jobs',
+            name: 'jobs',
+            component: JobsView,
+        },
+        {
+            path: '/jobs/add',
+            name: 'jobAdd',
+            component: JobAddView,
         },
     ]
 })
