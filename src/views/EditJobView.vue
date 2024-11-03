e96b792fe58db889e3cf4237eba76c46f7843d34
Parth Makwana <makwanaparth1820@gmail.com>
2024-11-03 14:36:08 +0530

    Add edit and delete job functionalities

--- src/views/EditJobView.vue
+++ src/views/EditJobView.vue
@@ -1,7 +1,8 @@
 <script setup>
 import router from '@/router'
 import axios from 'axios';
-import { reactive } from 'vue';
+import { onMounted, reactive } from 'vue';
+import { useRoute } from 'vue-router';
 import { useToast } from 'vue-toastification';
 
 const form = reactive({
@@ -17,10 +18,19 @@ const form = reactive({
         contactPhone: ''
     }
 })
+const route = useRoute()
+
 const toast = useToast()
 
+const jobId = route.params.id;
+
+const state = reactive({
+    job: {},
+    isLoading: true
+})
+
 const handleSubmit = async () => {
-    const newJob = {
+    const updatedJob = {
         title: form.title,
         type: form.type,
         location: form.location,
@@ -35,23 +45,45 @@ const handleSubmit = async () => {
     }
 
     try {
-        const response = await axios.post('/api/jobs', newJob);
+        const response = await axios.put(`/api/jobs/${jobId}`, updatedJob);
         // @todo-show toast
-        toast.success('Job Added Successfully')
+        toast.success('Job Updated Successfully')
         router.push(`/jobs/${response.data.id}`)
     } catch (error) {
         console.error("Error fetching job", error)
-        toast.error('Job Was Not Added');
+        toast.error('Job Was Not Updated');
     }
 
 }
+
+onMounted(async () => {
+    try {
+        const response = await axios.get(`/api/jobs/${jobId}`);
+        state.job = response.data;
+        //populate input
+        form.type = state.job.type;
+        form.title = state.job.title;
+        form.description = state.job.description;
+        form.salary = state.job.salary;
+        form.location = state.job.location;
+        form.company.name = state.job.company.name
+        form.company.description = state.job.company.description
+        form.company.contactEmail = state.job.company.contactEmail
+        form.company.contactPhone = state.job.company.contactPhone
+    } catch (error) {
+        console.error("Error Fetching job", error)
+    } finally {
+        state.isLoading = false;
+    }
+
+})
 </script>
 <template>
     <section class="bg-green-50">
         <div class="container m-auto max-w-2xl py-24">
             <div class="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
                 <form @submit.prevent="handleSubmit">
-                    <h2 class="text-3xl text-center font-semibold mb-6">Add Job</h2>
+                    <h2 class="text-3xl text-center font-semibold mb-6">Edit Job</h2>
 
                     <div class="mb-4">
                         <label for="type" class="block text-gray-700 font-bold mb-2">Job Type</label>
@@ -135,7 +167,7 @@ const handleSubmit = async () => {
                         <button
                             class="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline"
                             type="submit">
-                            Add Job
+                            Update Job
                         </button>
                     </div>
                 </form>
